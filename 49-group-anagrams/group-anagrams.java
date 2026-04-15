ab8059eaf924d36665f458e0a756618637c4db55
vtu26297-hub <vtu26297@veltech.edu.in>
2026-04-15 10:30:18 +0530

    Time: 7 ms (74.37%) | Memory: 49.7 MB (50.70%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
